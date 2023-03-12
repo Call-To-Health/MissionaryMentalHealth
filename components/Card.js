ad97eb47cd319130db1def4fc3b7f46821ff2840
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2023-03-12 10:45:29 -0600

    Search works!

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ import { CircleButton } from "./Button";
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 // import { fetchRandomDocs } from "../firebase";
 
-const Card = ({doc,randomDocs}) => {
+const Card = ({doc,randomDocs, filteredData}) => {
   const navigation = useNavigation();
 
   const handlePress = (story) => {
@@ -18,73 +18,70 @@ const Card = ({doc,randomDocs}) => {
   const imageSource = (`assets.missionaries${imageIndex}`);
   // console.log("imageSource:", imageSource);
 
-  console.log("Here is randomDocs as it is in the Card.js" + Object.values(randomDocs));
+  // console.log("Here is randomDocs as it is in the Card.js" + Object.values(randomDocs));
+
+  if (!doc) {
+    return null;
+  }
 
   return (
-    
-    <>
-      
-      {Object.values(randomDocs).length > 0 &&
-      Object.values(randomDocs).map((story) => (
-        <TouchableOpacity
-          key={story.id}
-          onPress={() => handlePress(story)}
-          >
-          <View
-          style={{
-            backgroundColor: COLORS.white,
-            borderRadius: SIZES.font,
-            marginBottom: SIZES.extraLarge,
-            margin: SIZES.base,
-            ...SHADOWS.dark,
-          }}
-        >
-          <View style={{ width: "100%", height: 80 }}>
-            <Image
-              // source={imageSource}
-              source={assets.missionaries1}
-              resizeMode="cover"
-              style={{
-                width: "100%",
-                height: "100%",
-                borderTopLeftRadius: SIZES.font,
-                borderTopRightRadius: SIZES.font,
-              }}
-            />
+    <TouchableOpacity
+      key={doc.id}
+      onPress={() => handlePress(doc)}
+    >
+      <View
+        style={{
+          backgroundColor: COLORS.white,
+          borderRadius: SIZES.font,
+          marginBottom: SIZES.extraLarge,
+          margin: SIZES.base,
+          ...SHADOWS.dark,
+        }}
+      >
+        <View style={{ width: "100%", height: 80 }}>
+          <Image
+            // source={imageSource}
+            source={assets.missionaries1}
+            resizeMode="cover"
+            style={{
+              width: "100%",
+              height: "100%",
+              borderTopLeftRadius: SIZES.font,
+              borderTopRightRadius: SIZES.font,
+            }}
+          />
 
-            {/* <CircleButton
+          {/* <CircleButton
               imgUrl={assets.heart}
               right={10}
               top={10}
               handlePress={() => handlePress(story.id)}
             /> */}
-          </View>
+        </View>
 
-          <SubInfo date={story?.date} location={story?.location} />
+        <SubInfo date={doc?.date} location={doc?.location} />
 
-          <View style={{ width: "100%", padding: SIZES.font }}>
-            <Title
-              title={story.solution}
-              titleSize={SIZES.large}
-              subTitleSize={SIZES.small}
-            />
+        <View style={{ width: "100%", padding: SIZES.font }}>
+          <Title
+            title={doc.solution}
+            titleSize={SIZES.large}
+            subTitleSize={SIZES.small}
+          />
 
-            <View
-              style={{
-                marginTop: SIZES.font,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
-              }}
-            >
-              <Text>by {story.name}</Text>
-              <TaggedItems tags={story.tag} />
-            </View>
+          <View
+            style={{
+              marginTop: SIZES.font,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Text>by {doc.name}</Text>
+            <TaggedItems tags={doc.tag} />
           </View>
         </View>
-        </TouchableOpacity>
-      ))}
-    </>
+      </View>
+    </TouchableOpacity>
   );
 };
 
