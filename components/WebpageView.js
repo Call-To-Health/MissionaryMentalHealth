4d1b7209be034b0ea579d1aacf9c1a4d185ff96b
Shayna Oh <oh.shayna.ty@gmail.com>
2023-03-14 09:27:47 -0600

    Finished fixing the webview

--- components/WebpageView.js
+++ components/WebpageView.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {WebView} from "react-native-webview";
+import { SafeAreaView } from 'react-native';
+import { WebView } from "react-native-webview";
+import { useNavigation } from '@react-navigation/native';
 
 //webURL should be a single string url
 export const WebpageView = (webURL) => {
-
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <WebView 
-            source={{ uri: JSON.stringify(webURL) }} 
+                source={{ uri: webURL.webURL }} 
             />
         </SafeAreaView>
     );
