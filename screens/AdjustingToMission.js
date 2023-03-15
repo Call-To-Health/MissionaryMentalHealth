5c2a58ff5faecc6f18fcea0f59432ca0999370bf
Shayna Oh <oh.shayna.ty@gmail.com>
2023-03-14 19:58:35 -0600

    put the back arrow in the component

--- screens/AdjustingToMission.js
+++ screens/AdjustingToMission.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { View, SafeAreaView, StatusBar, Text } from 'react-native';
-import { WebView } from "react-native-webview";
 import { CircleButton, FocusedStatusBar } from '../components';
-import { assets } from '../constants';
+import { COLORS } from '../constants';
 import { useNavigation } from '@react-navigation/native';
 import { WebpageView } from "../components/WebpageView";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -15,16 +14,6 @@ const AdjustingToMission = () => {
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <FocusedStatusBar
-                barStyle="dark-content"
-                backgroundColor="transparent"
-                translucent={true}
-            />
-            <View style={{ margin: 10, marginTop: 30 }}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Ionicons name="chevron-back" size={32} color="black" />
-                </TouchableOpacity>
-            </View>
             <WebpageView 
                 webURL="https://www.churchofjesuschrist.org/study/manual/resource-booklet-adjusting-to-missionary-life/introduction?lang=eng" 
                 style={{ zIndex: 0, flex: 1 }}
