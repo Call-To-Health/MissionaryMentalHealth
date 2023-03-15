095a94d04588f6ebdd00b48f55bd778d84feabdf
Shayna Oh <oh.shayna.ty@gmail.com>
2023-03-14 20:01:18 -0600

    deleting extra imports

--- screens/AdjustingToMission.js
+++ screens/AdjustingToMission.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import { View, SafeAreaView, StatusBar, Text } from 'react-native';
-import { CircleButton, FocusedStatusBar } from '../components';
-import { COLORS } from '../constants';
-import { useNavigation } from '@react-navigation/native';
 import { WebpageView } from "../components/WebpageView";
-import { createStackNavigator } from "@react-navigation/stack";
-import { TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 
 const AdjustingToMission = () => {
-    const navigation = useNavigation();
-    const Stack = createStackNavigator();
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
