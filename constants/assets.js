694559a2e6c320cd2cf42c89d9d3b0b10dcfd2c8
Benjamin Kloepfer <90466204+benjark@users.noreply.github.com>
2023-04-03 08:31:02 -0600

    Update assets.js

--- constants/assets.js
+++ constants/assets.js
@@ -16,8 +16,6 @@ import missionaries4 from "../assets/images/missionaries4.jpeg";
 import missionaries5 from "../assets/images/missionaries5.jpeg";
 import missionaries6 from "../assets/images/missionaries6.jpg";
 import missionaries7 from "../assets/images/missionaries7.jpeg";
-import Christ from "../assets/images/Christ.png";
-import journal from "../assets/images/journal.png";
 
 import person00 from "../assets/images/person00.png";
 import person01 from "../assets/images/person01.png";
@@ -49,7 +47,6 @@ export default {
   missionaries5,
   missionaries6,
   missionaries7,
-  Christ,
   journal,
 
   person00,
